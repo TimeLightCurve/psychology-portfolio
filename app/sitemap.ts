28cab322945c585071b6815ae3868af17cb506b2
unknown <shayan.shayeganfar@gmail.com>
2026-09-18 20:16:14 +0330

    indexing sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,28 @@
 import type { MetadataRoute } from 'next'
+import { getItems } from '@/lib/content'
+import type { BlogDocument } from '@/lib/models/blog'
+import type { ServiceDocument } from '@/lib/models/service'
 
 const siteUrl = 'https://www.shaghayeghzarei.ir'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return ['', '/about', '/blog', '/contact', '/services'].map((path) => ({
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const [services, blogs] = await Promise.all([
+    getItems<ServiceDocument>('services'),
+    getItems<BlogDocument>('blogs'),
+  ])
+
+  const staticPages = ['', '/about', '/blog', '/contact'].map((path) => ({
     url: `${siteUrl}${path}`,
     lastModified: new Date(),
   }))
+
+  const servicePages = services.map((service) => ({
+    url: `${siteUrl}/services/${service.slug}`,
+  }))
+
+  const blogPages = blogs.map((blog) => ({
+    url: `${siteUrl}/blog/${blog.slug}`,
+  }))
+
+  return [...staticPages, ...servicePages, ...blogPages]
 }
